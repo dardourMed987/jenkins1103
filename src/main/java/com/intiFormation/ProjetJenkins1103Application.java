a7a9ddcd7b377cd3aec9dc417f51ec101fa2c0c8
Dardour Mohammed <dardour.mohammed@gmail.com>
2024-03-11 12:44:30 +0000

    premier commit

---
+++ src/main/java/com/intiFormation/ProjetJenkins1103Application.java
@@ -0,0 +1,13 @@
+package com.intiFormation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJenkins1103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJenkins1103Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/intiFormation/TestController.java
@@ -0,0 +1,15 @@
+package com.intiFormation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/test")
+	public String test()
+	{
+		return "bonjour jenkins mac";
+	}
+
+}
